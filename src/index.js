7c13e413015ddd449ee2dff2de9c80a7deaa5631
algoroot <algoroot524@gmail.com>
2022-04-25 02:05:21 +0900

    Create ScrollToTop component

--- src/index.js
+++ src/index.js
@@ -7,11 +7,14 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/configStore";
 
+import ScrollOnTop from "./components/ScrollToTop";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
+        <ScrollOnTop />
         <App />
       </BrowserRouter>
     </Provider>
